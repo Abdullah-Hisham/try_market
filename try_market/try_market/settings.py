99cf6a1de03714ac475761bec77d3ad1cb8c1840
Abdullah_Hisham <abdulahhisham15@gmail.com>
2023-09-08 23:54:19 +0300

    fgr2

--- try_market/try_market/settings.py
+++ try_market/try_market/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*70id52+)t*r&p%_tm_f-!t&n09fk+1bnqunsu9gzw6ktiaq$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
