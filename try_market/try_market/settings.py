6e9b620ec202561ad54c41409dbd218e87878b04
Abdullah_Hisham <abdulahhisham15@gmail.com>
2023-09-08 23:48:06 +0300

    rr2

--- try_market/try_market/settings.py
+++ try_market/try_market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*70id52+)t*r&p%_tm_f-!t&n09fk+1bnqunsu9gzw6ktiaq$f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dfdgdfs.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
